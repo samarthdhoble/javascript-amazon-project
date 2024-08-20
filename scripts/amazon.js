72cf2e2b782ddc64ac2870f94a3a07ca08a59c15
Samarth Dhoble <samarthdhoble2005@gmail.com>
2024-08-20 14:08:35 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -85,7 +85,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       });
     }
 
+    let cartQuantity = 0;
 
-    console.log(cart);
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+
   });
 });
