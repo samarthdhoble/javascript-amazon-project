6f189e25cc3291a8c3af7b0ec0f79cb04ce3127d
Samarth Dhoble <samarthdhoble2005@gmail.com>
2024-08-20 15:31:41 +0530

    added utils for money conversion

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 
 let cartSummaryHTML = '';
@@ -32,7 +33,7 @@ cart.forEach((cartItem) => {
                 Black and Gray Athletic Cotton Socks - 6 Pairs
             </div>
             <div class="product-price">
-                $${(matchingProduct.priceCents / 10).toFixed(2)}
+                $${formatCurrency(matchingProduct.priceCents)}
             </div>
             <div class="product-quantity">
                 <span>
