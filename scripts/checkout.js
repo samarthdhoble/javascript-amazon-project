a7b69cb741a110270431c54f033ff2ad81349eaa
Samarth Dhoble <samarthdhoble2005@gmail.com>
2024-12-23 22:37:29 +0530

    create OOP version of cart.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/ordersummary.js";
 import { getProduct } from "../data/products.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
+import '../data/cart-oop.js'
 
 renderOrderSummary();
 renderPaymentSummary();
