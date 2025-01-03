41e88c788d14a4792a0f10c036db9fda04cc0208
Samarth Dhoble <samarthdhoble2005@gmail.com>
2025-01-03 22:45:57 +0530

    learn promises and convert code to promise

--- data/cart.js
+++ data/cart.js
@@ -90,3 +90,18 @@ export function addToCart(productId){
     saveToStorage();
 
   }
+
+
+  export function loadCart(fun){
+    const xhr = new XMLHttpRequest();
+  
+    xhr.addEventListener('load', () => {
+      console.log(xhr.response)
+      fun();
+    });
+  
+  
+    xhr.open('GET' , 'https://supersimplebackend.dev/cart')
+    xhr.send()
+  
+  };
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,14 +2,64 @@ import { renderOrderSummary } from "./checkout/ordersummary.js";
 import { getProduct } from "../data/products.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 import { loadProducts } from "../data/products.js";
-
+import { loadCart  } from "../data/cart.js";
 
 // import '../data/cart-class.js'
 // import '../data/backend-practice.js';
 
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('values');
+        });
+    
+    })
+    ,
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+])
+.then((values) => {
+    console.log(values)
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
+
+
+
+// LEARING PROMICES 
+
+// new Promise((resolve) => {
+//     loadProducts(() => {
+//         resolve();
+//     });
+
+// }).then(() => {
+
+//     return new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         });
+//     });
+
+// }).then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
+
+
+
+// CALL BACKS
+
+// loadProducts(() => {
+//     loadCart(() => {
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     });
+// });
 
